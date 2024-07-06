ea3147586b1e78f821260cfb9ba45bf1ec8b19ac
Atif Hussain <ah.atifhussain01@gmail.com>
2024-07-06 12:58:59 +0530

    Uploading Files of Amazon Clone
    After creating the Repo. I added all the files in it.

---
+++ Web.Amazon.in/Script.js
@@ -0,0 +1,5 @@
+// Loops
+let Name = prompt("Enter your name:");
+let email = prompt("Enter your email:");
+console.log("Welcome Mr",Name,".We will notify you about our new inovations on you email-", email, "-Thanks For having intrest on us");
+
